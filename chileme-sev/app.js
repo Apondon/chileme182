d49cb1461f47c87d5d1bef0d3d2e6a2797064872
apondon <apondon@126.com>
2020-04-22 15:34:41 +0800

    测试router

--- chileme-sev/app.js
+++ chileme-sev/app.js
@@ -3,30 +3,16 @@ const app = new Koa()
 const cfg = require('./config') // 引入配置文件
 const mongoose = require('mongoose') //引入mongoose
 const KoaBody = require('koa-body') //解析post请求
-
+const router = require('./router/router') // 引入路由(接口)
 // 配置数据库链接
-mongoose.connect(cfg.path)
-mongoose.connection.on('error',()=>{
-    console.log('数据库链接错误')
-})
-mongoose.connection.once('open',() => {
-    console.log('数据库已连接')
-})
-
-
-
-
-
-app.use(async ctx=>{
-    ctx.body = 'hellooooooooooo'
-})
+// mongoose.connect(cfg.path)
+// mongoose.connection.on('error',()=>{
+//     console.log('数据库链接错误')
+// })
+// mongoose.connection.once('open',() => {
+//     console.log('数据库已连接')
+// })
 app.use(KoaBody()) // 配置koa-body
-
-// const Router = require('koa-router')
-// const router = new Router()
-// app
-//   .use(router.routes())
-//   .use(router.allowedMethods())
-
+app.use(router.routes()).use(router.allowedMethods()) // 配置路由
 app.listen(cfg.port)
 console.log(`server is running at http://127.0.0.1:${cfg.port}`)
--- chileme-sev/router/router.js
+++ chileme-sev/router/router.js
@@ -2,9 +2,16 @@
 // 2.对koa-router 进行实例化
 // 3.定义接口
 // 4.暴露接口
-
+const Router = require('koa-router') // 引入koa-router
+const router = new Router()  // 对路由进行实例化
 // 接口
-
+// test
+router.get('/',async ctx => {
+    ctx.body = 'hello'
+})
+router.get('/test',async ctx => {
+    ctx.body = 'test'
+})
 
 // 用户接口
 
@@ -13,3 +20,7 @@
 
 
 // 订单接口
+
+
+
+module.exports = router
