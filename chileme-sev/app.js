33886938b5bd12ca44c34e8be513c7b7ea401498
apondon <apondon@126.com>
2020-04-22 15:11:56 +0800

    链接mongodb

--- chileme-sev/app.js
+++ chileme-sev/app.js
@@ -1,7 +1,21 @@
 const Koa = require('koa')
 const app = new Koa()
-
+const cfg = require('./config') // 引入配置文件
+const mongoose = require('mongoose') //引入mongoose
 const KoaBody = require('koa-body') //解析post请求
+
+// 配置数据库链接
+mongoose.connect(cfg.path)
+mongoose.connection.on('error',()=>{
+    console.log('数据库链接错误')
+})
+mongoose.connection.once('open',() => {
+    console.log('数据库已连接')
+})
+
+
+
+
 
 app.use(async ctx=>{
     ctx.body = 'hellooooooooooo'
@@ -14,5 +28,5 @@ app.use(KoaBody()) // 配置koa-body
 //   .use(router.routes())
 //   .use(router.allowedMethods())
 
-app.listen(3000)
-console.log(`server is running at http://127.0.0.1:3000`)
+app.listen(cfg.port)
+console.log(`server is running at http://127.0.0.1:${cfg.port}`)
