2cbe2a68a343ac60361d1de53c9a69936f12b7c6
apondon <apondon@126.com>
2020-04-23 16:44:58 +0800

    删除about页面，创建注册页面

---
+++ chileme-ui/src/views/Register.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+    div.register
+        h1 我是注册页面
+</template>
+<script>
+export default {
+    data(){
+        return{
+            form:{
+                username:'',
+                password:'',
+                cfm:'',
+                moblie:''
+            }
+        }
+    },
+    methods:{
+        // 注册
+        registerHandle(){
+
+        },
+        // 重置
+        resetHandle(){
+
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
